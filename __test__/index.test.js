e88c90539f9c8b36820e17c6757b0fbe1b9bc94b
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-06-21 21:06:08 +0100

    Implement movement

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,29 @@
-const {rover, roverDisplacement,roverDirection } = require('../index.js')
+const {
+    rover,
+    roverDisplacement,
+    roverDirection,
+    roverCoordinates,
+    roverMovement
+ } = require('../index.js')
 const command = ["f",10]
+const initialPosition = [0,0]
 describe("Rover orientation", () => {
     it("Giving an initial command, return command", () => {
         expect(rover(command)).toEqual(command);
     })
-    it ("Giving an initial command, returns direction and movemente", () => {
+    it ("Giving an initial command, returns displacement", () => {
         expect(roverDisplacement(command)).toEqual(10)
     });
+    it("Giving an initial command, returns position", () => {
+        expect(roverDirection(command)).toEqual("f")
+    })
+})
 
-}
-)
+describe("Rover movement", () => {
+    it("Giving an initial coordinate, return the rover position", () => {
+        expect(roverCoordinates(initialPosition)).toEqual(initialPosition)
+    })
+    it("Giving a command, return the position", () => {
+        expect(roverMovement(initialPosition,command)).toEqual([0,10])
+    })
+})
--- index.js
+++ index.js
@@ -8,11 +8,61 @@ function roverDisplacement(command){
     return displacement
 }
 
-function roverDirection(){
+function roverDirection(command){
     const direction = command[0]
     return direction
 }
+
+function roverCoordinates(position){
+    const position_x = position[0]
+    const position_y = position[1]
+    return [position_x,position_y]
+}
+
+function roverX(position){
+    const position_x = position[0]
+    return position_x
+}
+
+function roverY(position){
+    const position_y = position[1]
+    return position_y
+}
+
+function roverMovement(position,command){
+    //En que direccion es el movimento?
+    let x = roverX(position)
+    let y = roverY(position)
+    const dir = roverDirection(command)
+    const dis = roverDisplacement(command)
+    switch(dir) {
+        case "f": 
+        y+=dis
+        console.log('se mueve hacia adelante')
+        break
+        case "b":
+        y-=dis
+        console.log('se mueve hacia atrás')
+        break
+        case "l":
+        x-=dis
+        console.log('se mueve hacia la izquierda')
+        break;
+        case "r":
+        x+=dis
+        console.log('se mueve hacia la derecha')
+        break;
+    }
+    //Cuanto quieres que me mueva?
+    return [x,y]
+    //Dame tu nueva posicion
+
+}
+
 module.exports = {
     rover,
-    roverDisplacement
+    roverDisplacement,
+    roverDirection,
+    roverCoordinates,
+    roverMovement
 }
